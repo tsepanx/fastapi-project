d4d34b9d73027e34a100c6063db41e39cf6146a5
tsepanx <77626319+tsepanx@users.noreply.github.com>
2021-10-21 18:59:13 +0300

    Test for Create/Retrieve API

---
+++ backend/tests.py
@@ -0,0 +1,25 @@
+import requests, json
+import random
+
+
+def test_get_set():
+    hostname = "0.0.0.0"
+    port = 8001
+
+    post_url = f'http://{hostname}:{port}/shorten'
+
+    url = str(random.randint(100, 1000))
+    desc = url + 'desc'
+
+    data = {
+        'url': url,
+        'description': desc
+    }
+
+    j = requests.post(post_url, json.dumps(data)).json()
+
+    get_url = f'http://{hostname}:{port}/id/' + j['id']
+
+    r = requests.get(get_url(id)).json()
+
+    assert j == r
